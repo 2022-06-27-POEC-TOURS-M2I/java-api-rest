4a7ff1ee32870f73b63b94ccf7dad99736fdc1bf
Tiity <cadet_a@etna-alternance.net>
2022-08-24 10:08:46 +0200

    Start annuaire TP : getAll and create done

---
+++ src/main/java/fr/m2i/javaapirest/tpannuaire/AnnuaireDAO.java
@@ -0,0 +1,28 @@
+package fr.m2i.javaapirest.tpannuaire;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnnuaireDAO {
+
+    private List<Personne> personnes;
+    private Long nextId;
+
+    public AnnuaireDAO() {
+        this.personnes = new ArrayList();
+        this.nextId = 1L;
+    }
+
+    public Personne create(Personne personne) {
+        personne.setId(nextId);
+        personnes.add(personne);
+
+        nextId++;
+
+        return personne;
+    }
+
+    public List<Personne> getPersonnes() {
+        return personnes;
+    }
+}
